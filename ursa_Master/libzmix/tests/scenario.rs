93c0ca59fb07aaac2f55329263c1e4525d844ee2
sqiao22 <61515650+sqiao22@users.noreply.github.com>
2020-09-13 17:07:41 -0400

    Added GSign and GVerify

--- ursa_Master/libzmix/tests/scenario.rs
+++ ursa_Master/libzmix/tests/scenario.rs
@@ -11,15 +11,16 @@ use zmix::signatures::ps::prelude::*;
 
 use amcl_wrapper::field_elem::{FieldElement, FieldElementVector};
 
-use ursa::signatures::ed25519::*;
-
 use std::hash::{Hash, Hasher};
 use std::collections::hash_map::DefaultHasher;
 
 use amcl_wrapper::extension_field_gt::GT;
 
 
-use amcl_wrapper::ECCurve::big::BIG;
+
+use std::mem::transmute;
+
+// use amcl_wrapper::ECCurve::big::BIG;
 
 // use ursa::cl::issuer::Issuer;
 // use ursa::cl::prover::Prover;
@@ -27,7 +28,7 @@ use amcl_wrapper::ECCurve::big::BIG;
 // use ursa::cl::*;
 // use std::time::{Duration, Instant};
 
-
+//Create gpk, gmsk
 pub fn GSetup (count_msgs: usize, label: &[u8])->(Gpk, Gmsk){
     println!("GSetup Start.........");
     let (gpk, gmsk) = For_GSetup(count_msgs, label);
@@ -36,6 +37,7 @@ pub fn GSetup (count_msgs: usize, label: &[u8])->(Gpk, Gmsk){
     (gpk, gmsk)
 }
 
+//Create usk[i] and upk[i] for Gjoin 
 pub fn PKIJoin (count_msgs: usize, label: &[u8])->(PublicKey,SecretKey){
     println!("PKIJoin Start.........");
     let (upk_i, usk_i) = keygen(count_msgs, label);
@@ -47,96 +49,34 @@ pub fn PKIJoin (count_msgs: usize, label: &[u8])->(PublicKey,SecretKey){
     (upk_i, usk_i)
 }
 
-//Need to convert τ into number so it can be signed
-pub fn hashing(s: DefaultHasher,message: amcl_wrapper::group_elem_g1::G1)->(u64){
+//Need to convert G1 into number so it can be signed
+pub fn hashing(s: DefaultHasher,message: amcl_wrapper::group_elem_g1::G1)->u64{
     let mut hasher = s.clone();
     message.hash(&mut hasher);
     hasher.finish()
 }
 // Need this so tow can be a FieldElementVector
-pub fn sign_usk_i(s:DefaultHasher,tow:amcl_wrapper::group_elem_g1::G1,usk_i:SecretKey, upk_i:PublicKey)->(Signature){
-    let tow_hash=hashing(s.clone(),tow.clone());
+pub fn sign_usk_i(s:DefaultHasher,tow:amcl_wrapper::group_elem_g1::G1,usk_i:SecretKey, upk_i:PublicKey)->Signature{
+    let tow_hash=hashing(s.clone(),tow.clone()).to_be_bytes();
     // println!("{:?}", tow_hash);
+    let oneMess = FieldElement::from_msg_hash(&tow_hash);
     let mut msg=FieldElementVector::new(0);
     // println!("{:?}", tow_hash % 20 );
-    for i in 0..20 {
-        if i==tow_hash % 20 {
-            msg.push(FieldElement::zero());
-        }
-        else{
-            msg.push(FieldElement::one());
-        }
-    }
+    msg.push(oneMess);
     // println!("{:?}", msg);
     Signature::new(msg.as_slice(), &usk_i, &upk_i).unwrap()
+
 }
 // Check sign_usk_i signature
-pub fn verify_usk_i(signature_usk_i: Signature,s:DefaultHasher,tow:amcl_wrapper::group_elem_g1::G1, upk_i:PublicKey)->(bool){
-    
-    let tow_hash=hashing(s.clone(),tow.clone());
-    let mut msg=FieldElementVector::new(0);
-    // println!("{:?}", tow_hash % 20 );
-    for i in 0..20 {
-        if i==tow_hash % 20 {
-            msg.push(FieldElement::zero());
-        }
-        else{
-            msg.push(FieldElement::one());
-        }
-    }
+pub fn verify_usk_i(signature_usk_i: Signature,s:DefaultHasher,tow:amcl_wrapper::group_elem_g1::G1, upk_i:PublicKey)->bool{
 
+    let tow_hash=hashing(s.clone(),tow.clone()).to_be_bytes();
+    let oneMess = FieldElement::from_msg_hash(&tow_hash);
+    let mut msg=FieldElementVector::new(0);
+    msg.push(oneMess);
+    // println!("{:?}", msg);
     let check=signature_usk_i.verify(msg.as_slice(),&upk_i).unwrap();
     check
-}
-
-pub fn GJoin (i: usize, gpk: Gpk,gmsk: Gmsk, upk_i:PublicKey ,usk_i:SecretKey)->(){
-    println!("GJoin Start.........");
-    //USER generates a secret key,τ, τ_tidle, η and send τ, τ_tidle and η
-    println!("USER create ski, τ, τ_tidle and η and send τ, τ_tidle and η");
-    let ski= FieldElement::random();
-    let tow=&gpk.g * &ski;
-    let tow_tilde= &gpk.Y_tilde * &ski;
-    let mut s = DefaultHasher::new();
-    let n =sign_usk_i(s.clone(), tow.clone(), usk_i.clone(), upk_i.clone());
-    // let m =sign_usk_i(s.clone(), tow.clone(), usk_i.clone(), upk_i.clone());
-    // let check1=verify_usk_i(n.clone(),s.clone(), tow.clone(),upk_i.clone());
-    // let check2=verify_usk_i(m.clone(),s.clone(), tow.clone(),upk_i.clone());
-    // println!("{:?}",check1);
-    // println!("{:?}",check2);
-    
-
-    println!("GROUP MANAGER tests e(τ, Y_tilde) =e(g, τ_tilde)");
-    //GROUP MANAGER tests e(τ, Y_tilde) =e(g, τ_tilde)
-    let res = GT::ate_pairing(&tow, &gpk.Y_tilde);
-    let res2 = GT::ate_pairing(&gpk.g, &tow_tilde);
-    // println!("{:?}", res==res2);
-
-
-    println!("USER Start Proof of knowledge of ski");
-    //User start proof of knowledge for ski
-    // let pk=(&tow, &gpk.Y_tilde);
-    // test_PoK_multiple_sigs(pk,ski);
-    test_sigmaProtocol(gpk.g.clone(),ski.clone(),tow.clone());
-    
-
-    println!("Group Manager Generates u, σ");
-    //Group MANAGER u, σ←(σ1,σ2)←(gu,(gx·(τ)y)u) 
-    let u= FieldElement::random();
-    let sigma1=&gpk.g * &u;
-    let sigma2=&gpk.g * &gmsk.x + &tow * &gmsk.y * &u;
-    let sigma=(&sigma1,&sigma2);
-
-
-    println!("Group Manager Stores i,τ,η,τ_tilde and hash");
-    //Group Manager Store (i,τ,η,τ_tilde) need to add s for hasher
-    let secret_register=(i,tow,n,tow_tilde,s);
-
-    println!("USER Stores ski,σ,e(σ1,Y_tilde)");
-    //User Store (ski,σ,e(σ1,Y_tilde))
-    let gsk_i=(ski,sigma,GT::ate_pairing(&sigma1,&gpk.Y_tilde));
-
-    println!("GJoin Successful!");
-
 }
 
 //using sigma protocol, since PoKOfSignature requires vk and sk pair, when ski is the only thing given
@@ -157,135 +97,211 @@ pub fn test_sigmaProtocol(g:amcl_wrapper::group_elem_g1::G1,y:FieldElement,Y:amc
 
 }
 
-// fn test_PoK_multiple_sigs(gpk: Gpk, gmsk: Gmsk) {
-//     // Prove knowledge of multiple signatures together (using the same challenge)
-//     let count_msgs = 5;
-//     let (vk, sk) = keygen(count_msgs, "test".as_bytes());
+//Without this the requester cannot sign and if there’s no signature then there’s nothing to verify
+pub fn GJoin (i: usize, gpk: Gpk,gmsk: Gmsk, upk_i:PublicKey ,usk_i:SecretKey)->((usize,amcl_wrapper::group_elem_g1::G1,Signature,amcl_wrapper::group_elem_g2::G2,DefaultHasher), (amcl_wrapper::field_elem::FieldElement, (amcl_wrapper::group_elem_g1::G1, amcl_wrapper::group_elem_g1::G1), amcl_wrapper::extension_field_gt::GT)){
+    println!("GJoin Start.........");
+    //USER generates a secret key,τ, τ_tidle, η and send τ, τ_tidle and η
+    println!("USER create ski, τ, τ_tidle and η and send τ, τ_tidle and η");
+    let ski= FieldElement::random();
+    let tow=&gpk.g * &ski;
+    let tow_tilde= &gpk.Y_tilde * &ski;
+    let mut s = DefaultHasher::new();
+    let n =sign_usk_i(s.clone(), tow.clone(), usk_i.clone(), upk_i.clone());
+    // let m =sign_usk_i(s.clone(), tow.clone(), usk_i.clone(), upk_i.clone());
+    // let check1=verify_usk_i(n.clone(),s.clone(), tow.clone(),upk_i.clone());
+    // let check2=verify_usk_i(m.clone(),s.clone(), tow.clone(),upk_i.clone());
+    // println!("{:?}",check1);
+    // println!("{:?}",check2);
+    
 
-//     let msgs_1 = FieldElementVector::random(count_msgs);
-//     let sig_1 = Signature::new(msgs_1.as_slice(), &sk, &vk).unwrap();
-//     assert!(sig_1.verify(msgs_1.as_slice(), &vk).unwrap());
-
-//     let msgs_2 = FieldElementVector::random(count_msgs);
-//     let sig_2 = Signature::new(msgs_2.as_slice(), &sk, &vk).unwrap();
-//     assert!(sig_2.verify(msgs_2.as_slice(), &vk).unwrap());
-
-//     let pok_1 =
-//         PoKOfSignature::init(&sig_1, &vk, msgs_1.as_slice(), None, HashSet::new()).unwrap();
-//     let pok_2 =
-//         PoKOfSignature::init(&sig_2, &vk, msgs_2.as_slice(), None, HashSet::new()).unwrap();
-
-//     let mut chal_bytes = vec![];
-//     chal_bytes.append(&mut pok_1.to_bytes());
-//     chal_bytes.append(&mut pok_2.to_bytes());
-
-//     let chal = FieldElement::from_msg_hash(&chal_bytes);
-
-//     let proof_1 = pok_1.gen_proof(&chal).unwrap();
-//     let proof_2 = pok_2.gen_proof(&chal).unwrap();
-
-//     assert!(proof_1.verify(&vk, HashMap::new(), &chal).unwrap());
-//     assert!(proof_2.verify(&vk, HashMap::new(), &chal).unwrap());
-// }
+    //GROUP MANAGER tests e(τ, Y_tilde) =e(g, τ_tilde)
+    let res = GT::ate_pairing(&tow, &gpk.Y_tilde);
+    let res2 = GT::ate_pairing(&gpk.g, &tow_tilde);
+    println!("GROUP MANAGER tests e(τ, Y_tilde) =e(g, τ_tilde): {:?}", res==res2);
+    // println!("{:?}", res==res2);
 
 
+    println!("USER Start Proof of knowledge of ski");
+    //User start proof of knowledge for ski
+    // let pk=(&tow, &gpk.Y_tilde);
+    // test_PoK_multiple_sigs(pk,ski);
+    test_sigmaProtocol(gpk.g.clone(),ski.clone(),tow.clone());
+    
 
+    println!("Group Manager Generates u, σ");
+    //Group MANAGER u, σ←(σ1,σ2)←(gu,(gx·(τ)y)u) 
+    let u= FieldElement::random();
+    let sigma1=&gpk.g * &u;
+    //(g^x·(τ)^y)^u=g^x^u·(τ)^y^u IS this true?????
+    let sigma2=&gpk.g * &gmsk.x * &u + &tow * &gmsk.y * &u;
+    let sigma=(sigma1.clone(),sigma2.clone());
+
+
+    println!("Group Manager Stores i,τ,η,τ_tilde and hash");
+    //Group Manager Store (i,τ,η,τ_tilde) need to add s for hasher
+    let secret_register=(i,tow,n,tow_tilde,s);
+
+    println!("USER Stores ski,σ,e(σ1,Y_tilde)");
+    //User Store (ski,σ,e(σ1,Y_tilde))
+    let gsk_i=(ski,sigma,GT::ate_pairing(&sigma1,&gpk.Y_tilde));
+
+    println!("GJoin Successful!");
+
+    (secret_register,gsk_i)
+
+}
+
+//Hash tuple of mess G1,GT, str
+pub fn H1(s:DefaultHasher,message:(amcl_wrapper::group_elem_g1::G1,
+    amcl_wrapper::group_elem_g1::G1,
+    amcl_wrapper::extension_field_gt::GT,&str))->u64{
+    let mut hasher = s.clone();
+    message.hash(&mut hasher);
+    hasher.finish()
+}
+
+// Requester sign message with ski[i] and outputs signature and message
+pub fn GSign(gsk_i:(amcl_wrapper::field_elem::FieldElement, 
+    (amcl_wrapper::group_elem_g1::G1, 
+        amcl_wrapper::group_elem_g1::G1),
+    amcl_wrapper::extension_field_gt::GT),msg:&'static  str)->(
+    (amcl_wrapper::group_elem_g1::G1, 
+    amcl_wrapper::group_elem_g1::G1, 
+    amcl_wrapper::field_elem::FieldElement, 
+    amcl_wrapper::field_elem::FieldElement), 
+    DefaultHasher,&'static  str){
+    println!("GSign Start.........");
+    // let msg="test_message";
+    let ski=gsk_i.0;
+    let sigma1=gsk_i.1.0;
+    let sigma2=gsk_i.1.1;
+    let e=gsk_i.2;
+
+    //USER Create t and  computing  (σ′1,σ′2)←(σt1,σt2)
+    let t = FieldElement::random();
+    let sigma1_dash=sigma1 * &t;
+    let sigma2_dash=sigma2 * &t;
+
+    //USER create a  signature  of  knowledge  ofski.
+    let k = FieldElement::random();
+    // e(σ′1, Y_tilde)^k←e(σ1, Y_tilde)^k·t
+    let e_tok_tot=e.pow(&k).pow(&t);
+
+    //Please note code need to convert (σ′1,σ′2,e(σ1, Y_tilde)^k·t,m) to a hash u8 so this tuple can be converted into Fieldelement form using from_msg_hash
+    let mut hash_saved = DefaultHasher::new();
+    let number = H1(hash_saved.clone(),(sigma1_dash.clone(),sigma2_dash.clone(),e_tok_tot.clone(),msg)).to_be_bytes();
+    // let number = let bytes: [u8; 4] = unsafe { transmute(H1((sigma1_dash.clone(),sigma2_dash.clone(),e_tok_tot.clone(),msg)).to_be()) };
+    // let number2 = H1(ss.clone(),(sigma1_dash.clone(),sigma2_dash.clone(),e_tok_tot.clone(),msg)).to_be_bytes();
+    // println!("{:?}", number);
+
+    //c needs to be a fieldElement
+    let c = FieldElement::from_msg_hash(&number);
+    // //make sure hash consistent
+    // let c2 = FieldElement::from_msg_hash(&number);
+    // println!("{:?}", c);
+    // println!("{:?}", c2);
+
+    // USER Compute s←k+c·ski
+    let s = &k + &c * &ski;
+
+    //Output outputs (σ′1,σ′2,c,s) and m
+    let mu=(sigma1_dash,sigma2_dash,c,s);
+    println!("GSign Successful!");
+
+    (mu,hash_saved.clone(), msg)
+
+}
+
+//Verify Requester Group ID
+pub fn GVerify(gpk: Gpk,mu:(amcl_wrapper::group_elem_g1::G1, 
+    amcl_wrapper::group_elem_g1::G1, 
+    amcl_wrapper::field_elem::FieldElement, 
+    amcl_wrapper::field_elem::FieldElement), 
+    hash_for_tuple:DefaultHasher,msg:&'static  str)->(){
+
+    println!("GVerify Start.........");
+    let sigma1_dash=mu.0;
+    let sigma2_dash=mu.1;
+    let c=mu.2;
+    let c1=c.clone();
+    let s=mu.3;
+
+    // Verifier computes R←(e(σ1^-1, X_tilde)·e(σ2, g_tilde))−c·e(σs1, Y_tilde) 
+    // let b = &-c; //also works, but slower?
+    let b =&c.negation();
+    //Assuming (e(g1,g2)*e(h1,h2))^-c ==e(g1^-c,g2)*e(h1^-c,h2)
+    let R =GT::ate_multi_pairing(vec![(&(-&sigma1_dash).scalar_mul_variable_time(b),&gpk.X_tilde),
+        (&sigma2_dash.scalar_mul_variable_time(b),&gpk.g_tilde),
+        (&sigma1_dash.scalar_mul_variable_time(&s),&gpk.Y_tilde)]);
+    let number = H1(hash_for_tuple.clone(),(sigma1_dash.clone(),sigma2_dash.clone(),R.clone(),msg)).to_be_bytes();
+    let c2 = FieldElement::from_msg_hash(&number);
+    // Verify that c=H(σ1,σ2,R,m);
+    println!("Does this Verify: {:?}", c1==c2);
+
+    println!("GVerify Successful!");
+
+
+    // a=e(σ1^-1, X_tilde)·e(σ2, g_tilde))^−c
+
+    // let a = GT::ate_2_pairing(&(-&sigma1_dash),&gpk.X_tilde,&sigma2_dash,&gpk.g_tilde).pow(&-c);
+    // // b=e(σ1^s, Y_tilde)=e(σ1, Y_tilde)^s;
+    // // let b = GT::ate_pairing(&sigma1_dash,&gpk.Y_tilde).pow(&s);
+    // let b=GT::ate_pairing(&sigma1_dash.scalar_mul_variable_time(&s),&gpk.Y_tilde);
+    // // R=a·b
+    // // let R = a*b;
+
+    // let b = &-c;
+
+
+    // let e_vector = Vec::new();
+
+    // let sig1_inverse=-&sigma1_dash;
+    // e_vector.push((sig1_inverse,gpk.X_tilde));
+
+    // e_vector.push((sigma2_dash,gpk.g_tilde));
+
+    // let sig1_to_s=sigma1_dash.scalar_mul_variable_time(&s);
+    // e_vector.push((sig1_to_s,gpk.Y_tilde));
+
+
+    // let a1 = GT::ate_2_pairing(&(-&sigma1_dash),&gpk.X_tilde,&sigma2_dash,&gpk.g_tilde).pow(&s);
+    // let a2 = GT::ate_2_pairing(&(-&sigma1_dash).scalar_mul_variable_time(&s),&gpk.X_tilde,&sigma2_dash.scalar_mul_variable_time(&s),&gpk.g_tilde);
+    // println!("{:?}", a1==a2);
+
+    //e(g1,g2)^s=e(g1^s,g2);
+    // let b2=GT::ate_pairing(&sigma1_dash.scalar_mul_variable_time(&s),&gpk.Y_tilde);
+    // println!("{:?}", b==b2);
+
+
+
+    // let test1=GT::ate_pairing(&(-&sigma1_dash),&gpk.Y_tilde);
+    // let test1_1=GT::ate_pairing(&sigma1_dash,&gpk.Y_tilde);
+    // let test2=GT::ate_pairing(&sigma1_dash,&gpk.Y_tilde).inverse();
+
+    // println!("{:?}", test1==test2);
+    // println!("{:?}", test1_1==test2);
+
+    // let r=ate_pairing();
+
+}
 
 #[test]
 fn test_scenario_1() {
-    // User request signer to sign 10 messages where signer knows only 8 messages, the other 2 are given in a form of commitment.
-    // Once user receives the signature, it engages in a proof of knowledge of signature with a verifier.
-    // The user also reveals to the verifier some of the messages.
     let count_msgs = 10;
-    let committed_msgs = 2;
     let label="test".as_bytes();
     let (gpk, gmsk) = GSetup(count_msgs,label);
 
-    let (upk_i, usk_i)=PKIJoin(20,label);
+    let (upk_i, usk_i)=PKIJoin(1,label);
 
-
-    // println!("{}",gpk.X_tilde);
-    // println!("{:?}",gpk.Y_tilde);
-    // println!("{:?}",gpk.Y);
-    // println!("{}",sk.X);
-    // println!("{:?}",gpk.g);
-    // println!("{:?}",gpk.g_tilde);
     let user_id=1;
-    GJoin (user_id,gpk.clone(),gmsk.clone(), upk_i,usk_i);
+
+    let (secret_register,gsk_i) = GJoin (user_id,gpk.clone(),gmsk.clone(), upk_i,usk_i);
+
+    //note gpk is used for debuging remove gpk parameter when in production
+    let (mu,hash_for_tuple, msg)= GSign(gsk_i.clone(),"I require 10 boeing 747");
+
+    let verified_result=GVerify(gpk,mu,hash_for_tuple, msg);
 
 
 
-
-    // let msgs = SignatureMessageVector::random(count_msgs);
-    // let blinding = SignatureMessage::random();
-
-    // // User commits to some messages
-    // let mut comm = SignatureGroup::new();
-    // for i in 0..committed_msgs {
-    //     comm += &gpk.Y[i] * &msgs[i];
-    // }
-    // comm += &gpk.g * &blinding;
-
-    // {
-    //     // User and signer engage in a proof of knowledge for the above commitment `comm`
-    //     let mut bases = Vec::<SignatureGroup>::new();
-    //     let mut hidden_msgs = Vec::<SignatureMessage>::new();
-    //     for i in 0..committed_msgs {
-    //         bases.push(gpk.Y[i].clone());
-    //         hidden_msgs.push(msgs[i].clone());
-    //     }
-    //     bases.push(gpk.g.clone());
-    //     hidden_msgs.push(blinding.clone());
-
-    //     // User creates a random commitment, computes challenge and response. The proof of knowledge consists of commitment and responses
-    //     let mut committing = ProverCommittingSignatureGroup::new();
-    //     for b in &bases {
-    //         committing.commit(b, None);
-    //     }
-    //     let committed = committing.finish();
-
-    //     // Note: The challenge may come from the main protocol
-    //     let chal = committed.gen_challenge(comm.to_bytes());
-
-    //     let proof = committed.gen_proof(&chal, hidden_msgs.as_slice()).unwrap();
-
-    //     // Signer verifies the proof of knowledge.
-    //     assert!(proof.verify(bases.as_slice(), &comm, &chal).unwrap());
-    // }
-
-    // // Get signature, unblind it and then verify.
-    // let sig_blinded = Signature::new_with_committed_messages(
-    //     &comm,
-    //     &msgs.as_slice()[committed_msgs..count_msgs],
-    //     &gmsk,
-    //     &gpk,
-    // )
-    // .unwrap();
-    // let sig_unblinded = sig_blinded.get_unblinded_signature(&blinding);
-    // assert!(sig_unblinded.verify(msgs.as_slice(), &gpk).unwrap());
-
-    // // Do a proof of knowledge of the signature and also reveal some of the messages.
-    // let mut revealed_msg_indices = HashSet::new();
-    // revealed_msg_indices.insert(4);
-    // revealed_msg_indices.insert(6);
-    // revealed_msg_indices.insert(9);
-
-    // let pok = PoKOfSignature::init(
-    //     &sig_unblinded,
-    //     &gpk,
-    //     msgs.as_slice(),
-    //     None,
-    //     revealed_msg_indices.clone(),
-    // )
-    // .unwrap();
-
-    // let chal = SignatureMessage::from_msg_hash(&pok.to_bytes());
-
-    // let proof = pok.gen_proof(&chal).unwrap();
-
-    // let mut revealed_msgs = HashMap::new();
-    // for i in &revealed_msg_indices {
-    //     revealed_msgs.insert(i.clone(), msgs[*i].clone());
-    // }
-    // assert!(proof.verify(&gpk, revealed_msgs.clone(), &chal).unwrap());
 }
